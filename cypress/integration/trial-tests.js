9043219e87440869888f910c1392ac591f5ce47e
sergeyrogachevsky <sergey.rogachevsky@solbeg.com>
2022-01-04 20:18:27 +0300

    Added menu, pageTitle and product list checks

--- cypress/integration/trial-tests.js
+++ cypress/integration/trial-tests.js
@@ -14,13 +14,8 @@ describe('Trial tests', () => {
             cy.get('input[type = "submit"]').should('have.value', 'Search');
         });
         Navigation.isHeaderLinksAppeared();
-
-    //   cy.get('.todo-list li').should('have.length', 2)
-    //   // We can go even further and check that the default todos each contain
-    //   // the correct text. We use the `first` and `last` functions
-    //   // to get just the first and last matched elements individually,
-    //   // and then perform an assertion with `should`.
-    //   cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    //   cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+        Navigation.isCategoiesMenuAppeared();
+        Navigation.isElemetAppeared(Navigation.pageTitleLocator);
+        Navigation.isProductListAppeared();
     })
 })
--- cypress/page-objects/navigation.page.js
+++ cypress/page-objects/navigation.page.js
@@ -1,19 +1,46 @@
 class Navigation {
-    static siteTitle = `Demo Web Shop. Apparel & Shoes`;
+  static siteTitle = `Demo Web Shop. Apparel & Shoes`;
+  static headerLinks = ['Register', 'Log in', 'Shopping cart', 'Wishlist'];   
+  static categoiesMenu = ['Books', 'Computers', 'Electronics', 'Apparel & Shoes', 'Digital downloads', 'Jewelry', 'Gift Cards'];
+  
+  static categoiesMenuLocator = '.header-menu > ul > li';
+  static pageTitleLocator = '.page-title';
+  static productsListLocator = '.product-grid > .item-box';
+  static productItemLocator = '.product-item';
 
-    static headerLinks = ['Register', 'Log in', 'Shopping cart', 'Wishlist'];
+  static isElemetAppeared(locator) {
+    cy.get(locator).should('be.visible');
+  };
 
-    static isHeaderLinksAppeared() {
-      cy.get('.header-links').within( () => {
-        this.headerLinks.forEach( (item) => {
-          cy.contains(item);
-        });
+  static isHeaderLinksAppeared() {
+    cy.get('.header-links').within( () => {
+      this.headerLinks.forEach( (item) => {
+        cy.contains(item);
       });
-    };
+    });
+  };
+
+  static isCategoiesMenuAppeared() {
+    cy.get(this.categoiesMenuLocator).eq(this.categoiesMenu.length);
+    cy.get(this.categoiesMenuLocator).within( () => {
+      this.categoiesMenu.forEach( (item) => {
+        cy.contains(item);
+      });
+    });
+  };
+
+  static isProductListAppeared() {
+    cy.get(this.productItemLocator).should('have.length', 8).each(() => {
+      cy.get('.picture');
+      cy.get('.details').children('h2.product-title>a').should('not.be.empty');
+      cy.get('.details').children('.product-rating-box').children().should('have.class', 'rating');
+      cy.get('.details').children('.add-info').children().should('have.class', 'prices')
+        .children('span').should('not.be.empty');
+      cy.get('.details').children('.add-info').children().should('have.class', 'buttons')
+        .children().should('have.value', 'Add to cart').and('be.visible');
+    });
+  };
   
-    static navigateCreativeAssets() {
-      return cy.get(HomePage.CreativeAssets, {timeout: 10000}).first().click();
-    }
-  }
+}
   
-  export default Navigation;
+export default Navigation;
